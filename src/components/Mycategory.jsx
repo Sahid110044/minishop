ee8535aa7ff61d161b3962d6f28059bba6c7475f
Sahid110044 <139684859+Sahid110044@users.noreply.github.com>
2025-02-21 14:49:40 +0530

    Update Mycategory.jsx
    added new functionality to the main filter functionality

--- src/components/Mycategory.jsx
+++ src/components/Mycategory.jsx
@@ -30,7 +30,7 @@ function Mycategory({product}) {
      {show &&  <Alert className=' w-50 bg-info-subtle'>Please use AddtoCard and click on the corner cataloge image</Alert>}
 
        <div className="d-flex flex-wrap justify-content-center">
-       <Button variant='outline-warning' className='text-dark' onClick={()=>dispatch(sortingItem(""))}>Clear Filter</Button>
+       <Button variant='outline-warning' className='text-dark' onClick={()=>dispatch(sortingItem(""),dispatch(searchItem("")))}>Clear Filter</Button>
        <Button variant='outline-warning' className='text-dark' onClick={()=>dispatch(sortingItem("jewelery"))}>jewelery</Button>
        <Button variant='outline-warning' className='text-dark' onClick={()=>dispatch(sortingItem("electronics"))}>electronics</Button>
        <Button variant='outline-warning' className='text-dark' onClick={()=>dispatch(sortingItem("women's clothing"))}>women's clothing</Button>
